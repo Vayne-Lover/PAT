93c57dcf2a2b5a484611e7752c60e8d7daaa29f6
Vayne-Lover <406378362@qq.com>
2016-06-10 11:25:00 +0800

    1016

---
+++ 1016/1016/main.cpp
@@ -0,0 +1,62 @@
+//
+//  main.cpp
+//  1016
+//
+//  Created by 姜况童 on 16/6/10.
+//  Copyright © 2016年 姜况童. All rights reserved.
+//
+
+#include <iostream>
+
+int main() {
+    //string a="";
+    //string b = "";
+    //vector<string> aout;
+    //vector<string> bout;
+    //string da = "";
+    //string db = "";
+    ////vector<string> in;
+    //int counta = 0;
+    //int countb = 0;
+    //cin >> a >> da >> b >> db;
+    //auto astart = a.begin();
+    //auto aend = a.end();
+    //auto bstart = b.begin();
+    //auto bend = b.end();
+    //while (astart != aend)
+    //{
+    //	if (*astart == *da.begin())
+    //		//in.push_back(string(*astart));
+    //		counta += 1;
+    //}
+    //if (counta)
+    //	aout.push_back("0");
+    //else
+    //{
+    //	for (int i = 0;i < counta;++i)
+    //		aout.push_back(da);
+    //}
+    //while (bstart != bend)
+    //{
+    //	if (*bstart == *db.begin())
+    //		//in.push_back(string(*astart));
+    //		countb += 1;
+    //}
+    //if (countb)
+    //	aout.push_back("0");
+    //else
+    //{
+    //	for (int i = 0;i < countb;++i)
+    //		aout.push_back(db);
+    //}
+    //for (auto i : aout)
+    //{
+    //	cout << i;
+    //}
+    //cout << endl;
+    //for (auto i : bout)
+    //{
+    //	cout << i;
+    //}
+    return 0;
+}
