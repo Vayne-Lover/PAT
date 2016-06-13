4876b9ba60669361d7b088205ad5784cde601ab6
Vayne-Lover <406378362@qq.com>
2016-06-13 09:49:29 +0800

    1021

--- 1021/1021/main.cpp
+++ 1021/1021/main.cpp
@@ -8,11 +8,11 @@
 
 #include<iostream>
 #include<string>
-#include<vector>
+//#include<vector>
 using namespace std;
 
 int main() {
-    vector<char> b(10, '0');//There should be '0' other than 0
+    int b[10] = {0};
     string c = "";
     cin >> c;
     for (auto i = begin(c);i != end(c);++i)
@@ -21,8 +21,29 @@ int main() {
     }
     for (int i = 0;i < 10;++i)
     {
-        if (b[i] != '0')
+        if (b[i]>0)
             cout << i << ":" << b[i] << endl;
     }
     return 0;
 }
+
+//#include<iostream>
+//#include<string>
+//#include<vector>
+//using namespace std;
+//
+//int main() {
+//    vector<char> b(10, '0');//There should be '0' other than 0
+//    string c = "";
+//    cin >> c;
+//    for (auto i = begin(c);i != end(c);++i)
+//    {
+//        b[*i-'0'] += 1;//49,50,51 so need to -'0'
+//    }
+//    for (int i = 0;i < 10;++i)
+//    {
+//        if (b[i] != '0')
+//            cout << i << ":" << b[i] << endl;
+//    }
+//    return 0;
+//}
