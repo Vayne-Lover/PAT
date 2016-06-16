124e26adf850735e61433218b395ed21f45e7131
Vayne-Lover <406378362@qq.com>
2016-06-16 09:50:29 +0800

    1026

--- 1026/1026/main.cpp
+++ 1026/1026/main.cpp
@@ -10,7 +10,7 @@
 //#include<vector>
 //#include<string>
 using namespace std;
-int GetSecond(int num1,int num2) {//should - first and then +=1
+int GetSecond(int num1,int num2) { //should - first and then +=1
     int result = 0;
     int num = num2-num1;
     if (num % 100 >= 50)
@@ -31,42 +31,37 @@ void GetResult(int num1,int num2) {
     //e = (num2 - num1) / 100;
     //if ((num2 - num1) % 100 >= 50)//Should be %
     //	e++;
-    //int hh = e / 3600;
-    //e -= hh * 3600;
-    //int mm = e / 60;
-    //e -= mm * 60;
-    //int ss = e;
-    //int mm = (e % 3600) / 60;
-    //int ss = (e % 3600) % 60;
-    //int ss = e % 60;//Just e%60
-    //printf("%02d:%02d:%02d",hh,mm,ss);
     int hh = e / 3600;
-    if (hh < 10)
-    {
-        cout << "0" << hh << ":";
-    }
-    else
-    {
-        cout << hh << ":";
-    }
     int mm = (e % 3600) / 60;
-    if (mm < 10)
-    {
-        cout << "0" << mm << ":";
-    }
-    else
-    {
-        cout << mm << ":";
-    }
-    int ss = e % 60;
-    if (ss < 10)
-    {
-        cout << "0" << ss << endl;
-    }
-    else
-    {
-        cout << ss << endl;
-    }
+    int ss = e % 60;//Just e%60
+    printf("%02d:%02d:%02d",hh,mm,ss);
+//    int hh = e / 3600;
+//    if (hh < 10)
+//    {
+//        cout << "0" << hh << ":";
+//    }
+//    else
+//    {
+//        cout << hh << ":";
+//    }
+//    int mm = (e % 3600) / 60;
+//    if (mm < 10)
+//    {
+//        cout << "0" << mm << ":";
+//    }
+//    else
+//    {
+//        cout << mm << ":";
+//    }
+//    int ss = e % 60;
+//    if (ss < 10)
+//    {
+//        cout << "0" << ss << endl;
+//    }
+//    else
+//    {
+//        cout << ss << endl;
+//    }
 }
 int main() {
     int a = 0;
