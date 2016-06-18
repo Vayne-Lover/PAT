a735e9555596862b1f75e06134e793badb7975ed
Vayne-Lover <406378362@qq.com>
2016-06-18 12:07:34 +0800

    1032

--- 1032/1032/main.cpp
+++ 1032/1032/main.cpp
@@ -5,11 +5,25 @@
 //  Created by 姜况童 on 16/6/17.
 //  Copyright © 2016年 Vayne-Lover. All rights reserved.
 //
+#include<iostream>
+#include<vector>
+using namespace std;
+int main() {
+    int max = 0;
+    int maxSum = 0;
+    int count = 0;
+    cin >> count;
+    vector<int> school(count,0);
+    vector<int> score(count,0);
+    for (int i = 0;i < count;++i)
+    {
+        cin >> school[i] >> score[i];
+    }
+    for (auto it : school)
+    {
+        if (it > max)
+            max = it;
+    }
 
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
     return 0;
 }
