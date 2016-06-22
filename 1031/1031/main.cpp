ace88d0c12f35c17f8330bd3ad7d74d52a64460d
Vayne-Lover <406378362@qq.com>
2016-06-22 10:34:36 +0800

    1031

--- 1031/1031/main.cpp
+++ 1031/1031/main.cpp
@@ -44,11 +44,74 @@ int main() {
         int checknumber = sum % 11;
         int resultNumber = 0;
         char resultChar = '0';
-
+        switch (checknumber)
+        {
+            case 0:
+                resultNumber = 1;
+                break;
+            case 1:
+                resultNumber = 0;
+                break;
+            case 2:
+                resultChar = 'X';
+                break;
+            case 3:
+                resultNumber = 9;
+                break;
+            case 4:
+                resultNumber = 8;
+                break;
+            case 5:
+                resultNumber = 7;
+                break;
+            case 6:
+                resultNumber = 6;
+                break;
+            case 7:
+                resultNumber = 5;
+                break;
+            case 8:
+                resultNumber = 4;
+                break;
+            case 9:
+                resultNumber = 3;
+                break;
+            default:
+                break;
+        }
+        if (*(newTemp.end() - 1) == 'X' || (*(newTemp.end() - 1) - '0') == resultNumber)
+        {
+            
+        }
+        else
+        {
+            oneFlag = true;
+            hasChanged = true;
+        }
+        if (oneFlag)
+        {
+            cout << newTemp << endl;
+        }
+        oneFlag = false;
+    }
+    //for (auto it1 :number)
+    //{
+    //	for (auto it2=it1.begin();it2!=it1.end();++it2)
+    //	{
+    //		if (*it2<'0'||)
+    //		{
+    //			
+    //		}
+    //	}
+    //	if (oneFlag)
+    //	{
+    //		cout << *it1.begin() << endl;
+    //	}
+    //}
     if (!hasChanged)
     {
         cout << allRight << endl;
     }
-    }
+    
     return 0;
 }
