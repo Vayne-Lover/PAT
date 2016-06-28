0dacc0bc45ce64f88416cf9b9ff8c60129983c89
Vayne-Lover <406378362@qq.com>
2016-06-28 10:16:46 +0800

    1036

--- 1036/1036/main.cpp
+++ 1036/1036/main.cpp
@@ -6,10 +6,43 @@
 //  Copyright © 2016 Vayne-Lover. All rights reserved.
 //
 
-#include <iostream>
-
+#include<iostream>
+#include<string>
+#include<vector>
+#include<iterator>
+using namespace std;
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    int count=0;
+    cin>>count;
+    char ch=' ';
+    cin>>ch;
+    char a[20]={' '};
+    for(int i=0;i<count;++i)
+    {
+        a[i]=ch;
+    }
+    string str1=a;
+    //cout<<str1;
+    string str2=str1;
+    for(auto it=str2.begin()+1;it!=str2.end()-1;++it)
+    {
+        *it=' ';
+    }
+    //cout<<str2;
+    int outTime=0;
+    if(count%2==0)
+    {
+        outTime=count/2-2;
+    }
+    else
+    {
+        outTime=(count+1)/2-2;
+    }
+    cout<<str1<<endl;
+    for(int j=0;j<outTime;++j)
+    {
+        cout<<str2<<endl;
+    }
+    cout<<str1<<endl;
     return 0;
 }
