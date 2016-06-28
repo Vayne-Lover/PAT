0bf6a518ba9d128986946decb840a00fbb6d07d7
Vayne-Lover <406378362@qq.com>
2016-06-28 10:15:37 +0800

    1036

---
+++ 1036/1036/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  1036
+//
+//  Created by 姜况童 on 6/27/16.
+//  Copyright © 2016 Vayne-Lover. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
