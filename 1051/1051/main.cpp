ae1f5cf107055c68c685d6e7399e857cbfefebca
Vayne-Lover <406378362@qq.com>
2016-07-01 13:50:34 +0800

    The right answer

--- 1051/1051/main.cpp
+++ 1051/1051/main.cpp
@@ -11,7 +11,7 @@
 #include<math.h>
 using namespace std;//If it is 0.0001 it will still be 0.0001 when use setprecision
 int main(int argc, const char * argv[]) {//Take Care when s1,s2-0<0.01//When the answer is 0 cout is 0 rather than 0.00!!!
-    // insert code here...
+    
     double a1=0.0f;
     double a2=0.0f;
     double b1=0.0f;
@@ -26,6 +26,32 @@ int main(int argc, const char * argv[]) {//Take Care when s1,s2-0<0.01//When the
     b2=r2*sin(p2);
     s1=(a1*a2-b1*b2);
     s2=(a1*b2+a2*b1);
+    if(fabs(s1)<0.01)
+    {
+        s1=0;
+    }
+    if(fabs(s2)<0.01)
+    {
+        s2=0;
+    }
+    if(s2 >= 0)
+        printf("%.2lf+%.2lfi\n", s1, s2);
+    else
+        printf("%.2lf-%.2lfi\n", s1, -s2);
+//    double a1=0.0f;
+//    double a2=0.0f;
+//    double b1=0.0f;
+//    double b2=0.0f;
+//    double s1=0.0f;
+//    double s2=0.0f;
+//    double p1,p2,r1,r2;
+//    cin>>r1>>p1>>r2>>p2;
+//    a1=r1*cos(p1);
+//    b1=r1*sin(p1);
+//    a2=r2*cos(p2);
+//    b2=r2*sin(p2);
+//    s1=(a1*a2-b1*b2);
+//    s2=(a1*b2+a2*b1);
 //    if(fabs(s1)<0.01f)
 //    {
 //        s1=0;
@@ -54,22 +80,49 @@ int main(int argc, const char * argv[]) {//Take Care when s1,s2-0<0.01//When the
 //        {
 //            cout<<"+"<<"0.00"<<"i"<<endl;
 //        }
-        if(s2>=0)
-        {
-            printf("%.2lf+%.2lfi",s1,s2);
-        }
-        else
-        {
-            printf("%.2lf-%.2lfi",s1,fabs(s2));
-        }
-        if(s2>=0)
-        {
-            printf("%.2lf+%.2lfi\n",s1,s2);
-        }
-        else
-        {
-            printf("%.2lf-%.2lfi\n",s1,-s2);
-        }
-
+//        if(s2>=0)
+//        {
+//            printf("%.2lf+%.2lfi",s1,s2);
+//        }
+//        else
+//        {
+//            printf("%.2lf-%.2lfi",s1,fabs(s2));
+//        }
+//        if(s2>=0)
+//        {
+//            printf("%.2lf+%.2lfi\n",s1,s2);
+//        }
+//        else
+//        {
+//            printf("%.2lf-%.2lfi\n",s1,-s2);
+//        }
+//    
+    //    if(s2>=0)
+    //    {
+    //        cout<<setprecision(3)<<s1<<"+"<<s2<<"i"<<endl;
+    //    }
+    //    else
+    //    {
+    //        cout<<setprecision(3)<<s1<<"-"<<-s2<<"i"<<endl;
+    //    }
+    
+    //    double s1=0.0f;
+    //    double s2=0.0f;
+    //    double p1,p2,r1,r2;
+    //    cin>>r1>>p1>>r2>>p2;
+    //    s1=(r1*r2)*cos(p1+p2);
+    //    s2=(r1*r2)*sin(p1+p2);
+    //    if(fabs(s1)<0.01)
+    //    {
+    //        s1=0;
+    //    }
+    //    if(fabs(s2)<0.01)
+    //    {
+    //        s2=0;
+    //    }
+    //    if(s2 >= 0)
+    //        printf("%.2lf+%.2lfi\n", s1, s2);
+    //    else
+    //        printf("%.2lf-%.2lfi\n", s1, -s2);
     return 0;
 }
