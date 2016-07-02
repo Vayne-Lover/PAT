a20ae54792ed44f4c4bf3b39cf76c016412593c7
Vayne-Lover <406378362@qq.com>
2016-07-02 14:50:04 +0800

    1009

--- 1009/1009/main.cpp
+++ 1009/1009/main.cpp
@@ -7,9 +7,43 @@
 //
 
 #include <iostream>
-
+#include<sstream>
+#include<string>
+#include<vector>
+#include<stack>
+using namespace std;
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    stack<string> sta;
+    string str,strTemp;
+    getline(cin,str);
+    stringstream strs(str);
+    while(strs>>strTemp)
+    {
+        sta.push(strTemp);
+    }
+    while(!sta.empty())//No " " when finished
+    {
+        string temp=sta.top();
+        sta.pop();
+        if(sta.empty())
+            cout<<temp;
+        else
+            cout<<temp<<" ";
+    }
     return 0;
+//    stack<string> str;
+//    string temp;
+//    string a1;
+//    cin>>a1;
+//    while (getchar()!='\n')
+//    {
+//    cin>>temp;
+//    str.push(temp);
+//    }
+//    while (!str.empty()) {
+//        cout<<str.top()<<" ";
+//        str.pop();
+//    }
+//    cout<<a1<<endl;
+//    return 0;
 }
