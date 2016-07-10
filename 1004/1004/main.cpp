0f966c3e8111fe1a44d4baf99f27e98a8e878fe2
Vayne-Lover <406378362@qq.com>
2016-07-10 15:49:04 +0800

    Use vector to solve this problem

--- 1004/1004/main.cpp
+++ 1004/1004/main.cpp
@@ -7,9 +7,39 @@
 //
 
 #include <iostream>
-
+#include <vector>
+#include <string>
+using namespace std;
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    int count=0;
+    cin>>count;
+    string str;
+    int temp=0;
+    vector<string> name;
+    vector<string> number;
+    vector<int> grade;
+    for (int i=0; i<count; ++i)
+    {
+        cin>>str;
+        name.push_back(str);
+        cin>>str;
+        number.push_back(str);
+        cin>>temp;
+        grade.push_back(temp);
+    }
+    int maxGradeNumber=0;
+    int minGradeNumber=0;
+    for (int j=0; j<count; ++j) {
+        if(grade[j]>grade[maxGradeNumber])
+        {
+            maxGradeNumber=j;
+        }
+        if(grade[j]<grade[minGradeNumber])
+        {
+            minGradeNumber=j;
+        }
+    }
+    cout<<name[maxGradeNumber]<<" "<<number[maxGradeNumber]<<endl;
+    cout<<name[minGradeNumber]<<" "<<number[minGradeNumber]<<endl;
     return 0;
 }
