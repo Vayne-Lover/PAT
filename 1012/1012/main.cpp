b6cd81c6e0bac045982817ee4191b40251af7236
Vayne-Lover <406378362@qq.com>
2016-09-02 11:05:24 +0800

    Add 1012.

---
+++ 1012/1012/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  1012
+//
+//  Created by 姜况童 on 9/2/16.
+//  Copyright © 2016 Vayne-Lover. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
