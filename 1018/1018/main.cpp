e6c58bc10e5df2d9ad7b0516ae2506759099be78
Vayne-Lover <406378362@qq.com>
2016-09-07 15:09:42 +0800

    Add 1018.

---
+++ 1018/1018/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  1018
+//
+//  Created by 姜况童 on 9/7/16.
+//  Copyright © 2016 Vayne-Lover. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
