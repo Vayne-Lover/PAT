456e5c7896c942322450618b0decfd8513ba45bb
Vayne-Lover <406378362@qq.com>
2016-09-08 13:12:32 +0800

    Add input unit.

--- 1018/1018/main.cpp
+++ 1018/1018/main.cpp
@@ -7,9 +7,24 @@
 //
 
 #include <iostream>
-
+#include <string>
+#include <vector>
+using namespace std;
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    int n;
+    cin>>n;
+    vector<string> str1,str2;
+    string temp;
+    for (int i=0; i<n; ++i)
+    {
+        cin>>temp;
+        str1.push_back(temp);
+        cin>>temp;
+        str2.push_back(temp);
+    }
+    for(auto it=str1.begin(),it2=str2.begin();it!=str1.end();++it,++it2)
+    {
+        cout<<*it<<" "<<*it2<<endl;
+    }
     return 0;
 }
