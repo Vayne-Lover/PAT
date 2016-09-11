e2692ffb7cc77f980e62e53d5ad96ecab66c6002
Vayne-Lover <406378362@qq.com>
2016-09-11 17:46:33 +0800

    Add max().

--- 1018/1018/main.cpp
+++ 1018/1018/main.cpp
@@ -10,13 +10,29 @@
 #include <string>
 #include <vector>
 using namespace std;
+int max(int a,int b,int c)
+{
+    int max=0;
+    max=a>b?a:b;
+    max=max>c?max:c;
+    return max;
+}
 int main(int argc, const char * argv[]) {
     int n;
     cin>>n;
     vector<string> str1,str2;
     string temp;
-    int win=0;
+    int wina=0;
+    int winb=0;
     int equal=0;
+    int aC=0;
+    int aJ=0;
+    int aB=0;
+    int bC=0;
+    int bJ=0;
+    int bB=0;
+    string maxa;
+    string maxb;
     for (int i=0; i<n; ++i)
     {
         cin>>temp;
@@ -31,11 +47,56 @@ int main(int argc, const char * argv[]) {
             equal+=1;
             continue;
         }
-        if((*it=="C"&&*it2=="J") || (*it=="J"&&*it2=="B") || (*it=="B"&&*it2=="C"))
+        if(*it=="C"&&*it2=="J")
         {
-            win+=1;
+            aC+=1;
+            wina+=1;
+        }
+        if(*it=="J"&&*it2=="B")
+        {
+            aJ+=1;
+            wina+=1;
+        }
+        if(*it=="B"&&*it2=="C")
+        {
+            aB+=1;
+            wina+=1;
+        }
+        
+        if(*it2=="C"&&*it=="J")
+        {
+            bC+=1;
+            winb+=1;
+        }
+        if(*it2=="J"&&*it=="B")
+        {
+            bJ+=1;
+            winb+=1;
+        }
+        if(*it2=="B"&&*it=="C")
+        {
+            bB+=1;
+            winb+=1;
         }
     }
-    cout<<win<<" "<<equal<<" "<<n-win<<endl;
+    if(max(aC,aJ,aB)==aC)
+        maxa="C";
+    if(max(aC,aJ,aB)==aJ)
+        maxa="J";
+    if(max(aC,aJ,aB)==aB)
+        maxa="B";
+    if(aC==aB)
+        maxa="C";
+    
+    if(max(bC,bJ,bB)==bC)
+        maxb="C";
+    if(max(bC,bJ,bB)==bJ)
+        maxb="J";
+    if(max(bC,bJ,bB)==bB)
+        maxb="B";
+    
+    cout<<wina<<" "<<equal<<" "<<max(aC,aJ,aB)<<endl;
+    cout<<winb<<" "<<equal<<" "<<max(bC,bJ,bB)<<endl;
+    cout<<maxa<<" "<<maxb<<endl;
     return 0;
 }
