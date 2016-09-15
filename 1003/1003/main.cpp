bd0aab6a6464f8a7f187eddb7bbc9b7f594dc5b1
Vayne-Lover <406378362@qq.com>
2016-09-15 10:22:00 +0800

    Add input.

--- 1003/1003/main.cpp
+++ 1003/1003/main.cpp
@@ -7,9 +7,22 @@
 //
 
 #include <iostream>
-
+#include <vector>
+#include <string>
+using namespace std;
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    int n;
+    cin>>n;
+    vector<string> str;
+    string temp;
+    for(int i=0;i<n;++i)
+    {
+        getline(cin,temp);
+        str.push_back(temp);
+    }
+    for(auto it=str.begin();it!=str.end();++it)
+    {
+        cout<<*it<<endl;
+    }
     return 0;
 }
