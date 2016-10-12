7658555a1485c9fe12a0432b046c252b8cdfcd7c
Vayne-Lover <406378362@qq.com>
2016-10-12 20:28:11 +0800

    Add Prime().

--- 1007/1007/main.cpp
+++ 1007/1007/main.cpp
@@ -2,14 +2,42 @@
 //  main.cpp
 //  1007
 //
-//  Created by 姜况童 on 10/9/16.
+//  Created by 姜况童 on 10/12/16.
 //  Copyright © 2016 Vayne-Lover. All rights reserved.
 //
 
 #include <iostream>
-
+using namespace std;
+bool Prime(int n)
+{
+    if(n<2)
+    {
+        return false;
+    }
+    for(int i=2;i*i<n;++i)
+    {
+        if(n%i==0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+int fun(int n)
+{
+    int count=0;
+    for(int i=1;i<n;++n)
+    {
+        if(Prime(i)&&Prime(i-2))
+        {
+            count++;
+        }
+    }
+    return count;
+}
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    int num=0;
+    cin>>num;
+    cout<<fun(num)<<endl;
     return 0;
 }
