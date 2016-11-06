dbf9af98d9fbb37048098570b4a5f26432cc4b4e
Vayne-Lover <406378362@qq.com>
2016-11-06 14:21:18 +0800

    Add 1014.

---
+++ 1014/1014/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  1014
+//
+//  Created by 姜况童 on 2016/11/6.
+//  Copyright © 2016年 Vayne-Lover. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
